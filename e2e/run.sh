6291f5f6f6ee45a875fbcf596bb98c89a5d50cdc
David Nguyen <hpcsc@users.noreply.github.com>
2020-01-02 20:06:49 +1100

    refactor e2e script
    - move expect scripts to sub folder
    - move common logic to a shared file
    - pass in path to executable to avoid rebuilding binary in CI. CI should test the same binary that is built and published

--- e2e/run.sh
+++ e2e/run.sh
@@ -2,25 +2,30 @@
 
 set -e
 
-export TERM=xterm-256color
+PATH_TO_EXECUTABLE=$1
+if [ -z "${PATH_TO_EXECUTABLE}" ]; then
+    PATH_TO_EXECUTABLE=bin/aws-profile
+    echo "=== path to executable is not found, rebuilding to ${PATH_TO_EXECUTABLE}"
+    go build -o bin/aws-profile
+fi;
 
-go build
+export TERM=xterm-256color
 
 rm -rf ./e2e/tmp && mkdir ./e2e/tmp
 cp ./e2e/config/* ./e2e/tmp
 
-./e2e/set-from-credentials.exp
+./e2e/expect/set-from-credentials.exp ${PATH_TO_EXECUTABLE}
 
-CURRENT_PROFILE=$(./aws-profile get --credentials-path ./e2e/tmp/credentials --config-path ./e2e/tmp/config)
+CURRENT_PROFILE=$(${PATH_TO_EXECUTABLE} get --credentials-path ./e2e/tmp/credentials --config-path ./e2e/tmp/config)
 EXPECTED=credentials_profile_2
 if [ "${CURRENT_PROFILE}" != "${EXPECTED}" ]; then
     echo "FAILED [set-from-credentials] Expected [${EXPECTED}], but got [${CURRENT_PROFILE}]"
     exit 1
 fi;
 
-./e2e/set-from-config.exp
+./e2e/expect/set-from-config.exp ${PATH_TO_EXECUTABLE}
 
-CURRENT_PROFILE=$(./aws-profile get --credentials-path ./e2e/tmp/credentials --config-path ./e2e/tmp/config)
+CURRENT_PROFILE=$(${PATH_TO_EXECUTABLE} get --credentials-path ./e2e/tmp/credentials --config-path ./e2e/tmp/config)
 EXPECTED='assuming profile config_profile_1'
 if [ "${CURRENT_PROFILE}" != "${EXPECTED}" ]; then
     echo "FAILED: [set-from-config] Expected [${EXPECTED}], but got [${CURRENT_PROFILE}]"
