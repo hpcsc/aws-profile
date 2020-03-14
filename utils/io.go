630367f7e50b10319a1656c915bf6967b6463ae1
David Nguyen <hpcsc@users.noreply.github.com>
2020-03-14 16:02:05 +1100

    Change location of cached caller identity file

--- utils/io.go
+++ utils/io.go
@@ -6,6 +6,7 @@ import (
 	"gopkg.in/ini.v1"
 	"io/ioutil"
 	"os"
+	"path"
 )
 
 func WriteToFile(file *ini.File, unexpandedFilePath string) {
@@ -22,17 +23,41 @@ func WriteToFile(file *ini.File, unexpandedFilePath string) {
 	ioutil.WriteFile(filePath, buffer.Bytes(), 0600)
 }
 
-const cachedCallerIdentityFile = "~/.aws-profile-cached-caller-identity"
+const awsProfileHome = "~/.aws-profile"
+const cachedCallerIdentityFileName =  "cached-caller-identity"
+
+func createCachedCallerIdentityFileIfNotExists() (string, error ) {
+	expandedHomeDir := ExpandHomeDirectory(awsProfileHome)
+
+	if _, createDirError := os.Stat(expandedHomeDir); os.IsNotExist(createDirError) {
+		makeDirError := os.Mkdir(expandedHomeDir, os.FileMode(0755))
+		if makeDirError != nil {
+			return "", makeDirError
+		}
+	}
+
+	return path.Join(expandedHomeDir, cachedCallerIdentityFileName), nil
+}
 
 func ReadCachedCallerIdentity() (string, error) {
-	callerIdentity, error := ioutil.ReadFile(ExpandHomeDirectory(cachedCallerIdentityFile))
-	if error != nil {
-		return "", error
+	cachedCallerIdentityFile, createError := createCachedCallerIdentityFileIfNotExists()
+	if createError != nil {
+		return "", createError
+	}
+
+	callerIdentity, readError := ioutil.ReadFile(cachedCallerIdentityFile)
+	if readError != nil {
+		return "", readError
 	}
 
 	return string(callerIdentity), nil
 }
 
 func WriteCachedCallerIdentity(callerIdentity string) error {
-	return ioutil.WriteFile(ExpandHomeDirectory(cachedCallerIdentityFile), []byte(callerIdentity), 0600)
+	cachedCallerIdentityFile, createError := createCachedCallerIdentityFileIfNotExists()
+	if createError != nil {
+		return createError
+	}
+
+	return ioutil.WriteFile(cachedCallerIdentityFile, []byte(callerIdentity), 0644)
 }
