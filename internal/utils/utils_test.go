d69d243b98e1760595067282978204304e7c77a0
David Nguyen <hpcsc@users.noreply.github.com>
2020-11-01 21:28:28 +1100

    Add tests for ExpandHomeDirectory

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -3,8 +3,23 @@ package utils
 import (
 	"github.com/stretchr/testify/assert"
 	"os"
+	"strings"
 	"testing"
 )
+
+func TestExpandHomeDirectory(t *testing.T) {
+	t.Run("return original path if it does not start with tilde", func (t *testing.T) {
+		output := ExpandHomeDirectory("/home/root/some-file")
+
+		assert.Equal(t, "/home/root/some-file", output)
+	})
+
+	t.Run("replace tilde with user home directory when path starts with tilde", func (t *testing.T) {
+		output := ExpandHomeDirectory("~/.aws/config")
+
+		assert.False(t, strings.HasPrefix(output, "~/"))
+	})
+}
 
 func TestGetEnvVariableOrDefault(t *testing.T) {
 	t.Run("return environment variable value if it is set", func (t *testing.T) {
