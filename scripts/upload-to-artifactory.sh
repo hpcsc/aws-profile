a3f726fece643728c9b09093dfeee4e2dfd81157
David Nguyen <hpcsc@users.noreply.github.com>
2021-02-07 09:09:28 +1100

    Set version property when uploading to artifactory

--- scripts/upload-to-artifactory.sh
+++ scripts/upload-to-artifactory.sh
@@ -7,3 +7,11 @@ jfrog rt upload \
         --url https://hpcsc.jfrog.io/artifactory \
         "${SOURCE_PATTERN}" \
         "${REPOSITORY}/${VERSION_NUMBER}/"
+
+jfrog rt sp \
+        --recursive \
+        --include-dirs \
+        --access-token "${ACCESS_TOKEN}" \
+        --url https://hpcsc.jfrog.io/artifactory \
+        "${REPOSITORY}/${VERSION_NUMBER}*" \
+        "version=${VERSION_NUMBER}"
