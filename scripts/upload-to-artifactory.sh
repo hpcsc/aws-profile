387cf5fbf1060a61fddfb2f88a56a471baee0b72
David Nguyen <hpcsc@users.noreply.github.com>
2021-02-06 23:37:39 +1100

    Change ci pipeline to upload to artifactory

--- scripts/upload-to-artifactory.sh
+++ scripts/upload-to-artifactory.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 jfrog rt upload \
         --access-token "${ACCESS_TOKEN}" \
         --url https://hpcsc.jfrog.io/artifactory \
-        'batect*' \
-        "${REPOSITORY}/${VERSION}/"
+        "${SOURCE_PATTERN}" \
+        "${REPOSITORY}/${VERSION_NUMBER}/"
